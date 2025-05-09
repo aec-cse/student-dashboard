a8226a5616cc1b3e42da1b9d969a4b7b7ba80be0
ROHIT MANOHAR GIRHE <101627373+Rohit-Girhe@users.noreply.github.com>
2025-05-09 23:46:31 +0530

    Update and rename Firebase-Config.js to AdminRegistration.js

--- AdminRegistration.js
+++ AdminRegistration.js
@@ -1,5 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,4 +15,4 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-</script>
+
